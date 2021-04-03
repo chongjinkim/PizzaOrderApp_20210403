5b660ee08419f49577c814572a584ee370994d42
김종진 <martia0118@gmail.com>
2021-04-03 13:59:57 +0900

    탭레이아웃 + 뷰페이지 + 프래그먼트 연동 완료  - 프래그먼트는 임시 상태로 구현

--- app/src/main/java/com/nepplus/pizzaorderapp_20210403/MainActivity.kt
+++ app/src/main/java/com/nepplus/pizzaorderapp_20210403/MainActivity.kt
@@ -2,15 +2,21 @@ package com.nepplus.pizzaorderapp_20210403
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.nepplus.pizzaorderapp_20210403.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mViewPagerAdapter :  MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+
     }
 
 
@@ -20,7 +26,12 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+        mainTabLayout.setupWithViewPager(mainViewPager)
     }
+
+
 
 
 }
